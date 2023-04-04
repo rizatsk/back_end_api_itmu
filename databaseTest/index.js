c05df4de40f310d1ef1366c36ade008db07cb8e9
rizatsk <rizatsakmir@gmail.com>
2023-04-04 22:17:18 +0700

    update

--- databaseTest/index.js
+++ databaseTest/index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const { Pool } = require("pg");
-const testConfig = require("../config/test.json");
 
-const pool_test = new Pool(testConfig);
+const pool_test = new Pool({
+    user: process.env.PGUSER_TEST,
+    password: process.env.PGPASSWORD_TEST,
+    host: process.env.PGHOST_TEST,
+    port: process.env.PGPORT_TEST,
+    database: process.env.PGDATABASE_TEST
+});
 
 module.exports = pool_test;
