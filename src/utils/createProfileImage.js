7782b57c5f06c2db1922550263fa97b9c0e31d07
rizatsk <rizatsakmir@gmail.com>
2023-05-31 14:12:34 +0700

    update email and profile image

--- src/utils/createProfileImage.js
+++ src/utils/createProfileImage.js
@@ -1,23 +1,23 @@
-const svgCaptcha = require('svg-captcha');
-const crypto = require('crypto');
+// const svgCaptcha = require('svg-captcha');
+// const crypto = require('crypto');
 
 function createProfileImage(initials) {
-    initials = initials.split(' ').slice(0, 2).map(word => word.charAt(0)).join('');
-    initials = initials.toUpperCase();
+  initials = initials.split(' ').slice(0, 2).map(word => word.charAt(0)).join('');
+  initials = initials.toUpperCase();
 
-    // generate captcha
-    const captcha = svgCaptcha.create({ size: 6, ignoreChars: '0o1i' });
+  // generate captcha
+  // const captcha = svgCaptcha.create({ size: 6, ignoreChars: '0o1i' });
 
-    // buat SVG sederhana
-    const svg = `
+  // buat SVG sederhana
+  const svg = `
     <svg width="200" height="200" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
       <rect width="200" height="200" fill="#0F3D3E" />
-      <text x="50%" y="60%" text-anchor="middle" font-size="80" fill="#FFFFFF">${initials}</text>
+      <text x="50%" y="60%" text-anchor="middle" font-size="80" fill="#FFFFFF" style="font-family: Arial;">${initials}</text>
     </svg>
   `;
 
-    // kembalikan URL data
-    return `data:image/svg+xml;base64,${Buffer.from(svg).toString('base64')}`;
+  // kembalikan URL data
+  return `data:image/svg+xml;base64,${Buffer.from(svg).toString('base64')}`;
 }
 
 module.exports = createProfileImage;
